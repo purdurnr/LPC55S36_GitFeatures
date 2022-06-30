652090b8567d0cee166b2d7c91f231893e81d82d
NXP <NXP@DESKTOP-M7BGA8D>
2022-06-30 15:22:09 -0700

    Thanks for the session!

--- LPC55S36_Project/source/LPC55S36_Project.c
+++ LPC55S36_Project/source/LPC55S36_Project.c
@@ -2,6 +2,13 @@
  * Copyright 2016-2022 NXP
  * All rights reserved.
  *
+ *
+ *
+ * ============
+ * Very useful feature! version managment can be a nightmare.
+ * Looking forward to trying this with a Linux SVN server instead of git.
+ * ============
+ *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
  *
