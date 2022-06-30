b8cb6e24154364a30784149bf19f70ebce8784f9
NXP <NXP@DESKTOP-M7BGA8D>
2022-06-30 17:20:11 -0500

    thanks.  my work branch

--- LPC55S36_Project/source/LPC55S36_Project.c
+++ LPC55S36_Project/source/LPC55S36_Project.c
@@ -1,7 +1,10 @@
 /*
  * Copyright 2016-2022 NXP
  * All rights reserved.
- *
+ *Thanks!
+ *Thanks! again
+ *Thanks!
+ *Thanks!hello
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
  *
